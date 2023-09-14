3a8dbba46fdb587889a0273c05dc7c2afba87c28
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-14 18:41:41 -0300

    new: creating utilities folder

---
+++ utilities/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class CreatedUpdatedAt(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        auto_created=True,
+        verbose_name='Data de criação'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        auto_created=True,
+        verbose_name='Data de atualização'
+    )
