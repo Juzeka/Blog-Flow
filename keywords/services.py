941f6222e5d4f10604bfd1b89293a40c4e9f1168
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-16 14:42:28 -0300

    new: create service create_multiple keywords

--- keywords/services.py
+++ keywords/services.py
@@ -12,3 +12,21 @@ class KeywordServices:
         serializer.save()
 
         return serializer
+
+    def create_multiple(self):
+        list_data = self.data_multiple['datas']
+        return_type = self.data_multiple['return_type']
+        return_list = list()
+
+        for data in list_data:
+            self.data = data
+            result = self.create()
+
+            if return_type == 'instance':
+                result = result.instance
+            elif return_type == 'data':
+                result = result.data
+
+            return_list.append(result)
+
+        return return_list
