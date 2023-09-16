b269a4c1191e9798cb7d36d248e461f93d9fec4f
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-16 14:43:16 -0300

    new: create test create_multiple keywords

--- tests/keywords/services.py
+++ tests/keywords/services.py
@@ -1,7 +1,20 @@
 from utilities.tests import TestCase
+from rest_framework.utils.serializer_helpers import ReturnDict
 from parameterized import parameterized
 from keywords.services import KeywordServices
 from keywords.serializers import KeywordSerializer
+from keywords.models import Keyword
+
+
+DATAS_CREATE_MULTIPLE = [{'name': 'key 4'}, {'name': 'key 3'}]
+CASE_CREATE_MULTIPLE = [
+    ({'datas': DATAS_CREATE_MULTIPLE, 'return_type': 'instance'}, Keyword),
+    ({'datas': DATAS_CREATE_MULTIPLE, 'return_type': 'data'}, ReturnDict),
+    (
+        {'datas': DATAS_CREATE_MULTIPLE, 'return_type': 'serializer'},
+        KeywordSerializer
+    ),
+]
 
 
 class keywordServicesTestCase(TestCase):
@@ -11,3 +24,12 @@ class keywordServicesTestCase(TestCase):
         serializer = KeywordServices(data=data).create()
 
         self.assertIsInstance(serializer, KeywordSerializer)
+
+    @parameterized.expand(CASE_CREATE_MULTIPLE)
+    def test_create_multiple(self, data_multiple, type_expected):
+        return_list = KeywordServices(
+            data_multiple=data_multiple
+        ).create_multiple()
+
+        self.assertIsInstance(return_list, list)
+        self.assertIsInstance(return_list[0], type_expected)
