e20e959592d32239d306bf7fa29ae409311a01b1
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-16 15:23:52 -0300

    new: register categories routes

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ prefix_v1 = 'api/v1'
 
 urlpatterns = [
     path(f'{prefix_v1}/accounts/', include('accounts.urls')),
+    path(f'{prefix_v1}/categories/', include('categories.urls')),
     path('', admin.site.urls)
 ]
