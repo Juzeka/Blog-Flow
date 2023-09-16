c97d8cb5187bd0f4c827d2351901d75f3482dbb7
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-16 18:30:31 -0300

    new: creating article admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from articles.models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'category', 'status', 'is_visible']
