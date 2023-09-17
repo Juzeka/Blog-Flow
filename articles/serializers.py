634933a3d9e53afeef563e8e7b52fa1220fd57ce
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-16 21:54:48 -0300

    new: creating serializer detail for article

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from articles.models import Article
+from categories.serializers import CategoryDetailSerializer
+from keywords.serializers import KeywordSerializer
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+
+class ArticleDetailSerializer(ArticleSerializer):
+    category = CategoryDetailSerializer()
+    keywords = KeywordSerializer(many=True, read_only=True)
