55a6fa72214ea97b13853e7672cffa8c23b475fc
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-17 01:44:12 -0300

    new: creating serializer detail full from article

--- articles/serializers.py
+++ articles/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from articles.models import Article
 from categories.serializers import CategoryDetailSerializer
 from keywords.serializers import KeywordSerializer
-
+from comments.serializers import CommentDetailSerializer
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,5 +11,15 @@ class ArticleSerializer(serializers.ModelSerializer):
 
 
 class ArticleDetailSerializer(ArticleSerializer):
-    category = CategoryDetailSerializer()
+    category = CategoryDetailSerializer(read_only=True)
     keywords = KeywordSerializer(many=True, read_only=True)
+
+
+class ArticleDetailFullSerializer(ArticleSerializer):
+    comments = CommentDetailSerializer(many=True, read_only=True)
+
+    class Meta(ArticleDetailSerializer.Meta):
+        fields = [
+            'id', 'title', 'subtitle', 'content', 'status', 'author',
+            'category', 'keywords', 'is_visible', 'comments'
+        ]
