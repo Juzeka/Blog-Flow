9cbfdcdba6993991f94481bfe9d5de29f7f5aecc
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-17 22:08:28 -0300

    new: creating loggings from console

--- core/settings.py
+++ core/settings.py
@@ -113,6 +113,32 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
+LOGLEVEL = config('DJANGO_LOGLEVEL', default='INFO', cast=str).upper()
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': LOGLEVEL,
+            'handlers': [
+                'console',
+            ],
+        },
+    },
+}
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
