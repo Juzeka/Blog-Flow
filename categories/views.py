6d224e72fae2f9e023c91443602b8383c1535bfe
Rafael Gomes <rafaelgomesalmeida@hotmail.com>
2023-09-20 18:17:54 -0300

    new: creating schema for categories

--- categories/views.py
+++ categories/views.py
@@ -19,10 +19,35 @@ from core.schema_parameters import REQUEST_BODY_CREATE_CATEGORY
     decorator=swagger_auto_schema(
         tags=['Categories'],
         operation_id='categories_create',
-        operation_summary='Criação de artigo.',
+        operation_summary='Criação de categoria.',
         request_body=REQUEST_BODY_CREATE_CATEGORY
     )
 )
+@method_decorator(
+    name='retrieve',
+    decorator=swagger_auto_schema(
+        tags=['Categories'],
+        operation_id='categories_retrieve',
+        operation_summary='Detalhe de categoria.'
+    )
+)
+@method_decorator(
+    name='update',
+    decorator=swagger_auto_schema(
+        tags=['Categories'],
+        operation_id='categories_update',
+        operation_summary='Edição de categoria.'
+    )
+)
+@method_decorator(
+    name='destroy',
+    decorator=swagger_auto_schema(
+        tags=['Categories'],
+        operation_id='categories_destroy',
+        operation_summary='Deleção de categoria.'
+    )
+)
+@method_decorator(name='retrieve', decorator=cache_page(60 * 10))
 @method_decorator(name='list', decorator=cache_page(60 * 10))
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
